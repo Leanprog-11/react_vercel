26f9d1b54958442888eb2656c7a9b518c2c36783
Leanprog-11 <leandrojpino1003@gmail.com>
2024-10-03 21:12:48 -0400

    add redireccion

--- app_react/src/App.jsx
+++ app_react/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import { UserList } from './components/UserList';
 import { UseForm } from './components/UseForm';
 
@@ -9,6 +9,7 @@ function App() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/user-list" />} />
                 <Route path="/user-list" element={<UserList usuarios={usuarios} setUsuarios={setUsuarios} />} />
                 <Route path="/add" element={<UseForm usuarios={usuarios} setUsuarios={setUsuarios} />} />
                 <Route path="/edit/:id" element={<UseForm usuarios={usuarios} setUsuarios={setUsuarios} />} />
